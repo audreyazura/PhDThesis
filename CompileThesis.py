169b25de78a3ed57f9f93b5b8c2f463dc7acb629
audreyazura <azura.xiloscient@gmail.com>
2017-04-26 23:22:13 +0900

    Going to biblatex and PRL style

--- CompileThesis.py
+++ CompileThesis.py
@@ -14,7 +14,9 @@ pictureStr = re.compile(r"Pictures/")
 addrStr = re.compile(r"\.\./")
 newlineStr = re.compile(r"\n")
 usepackageStr = re.compile(r'\\usepackage(\[[\w=,]{1,50}\])?\{')
-biblioStr = re.compile(r'\\biblio')
+biblioStr = re.compile(r'[b,B]iblio')
+printbibStr = re.compile(r'\\printbiblio')
+bibresStr = re.compile(r'\\addbibres')
 begindocStr = re.compile(r'\\begin{doc')
 enddocStr = re.compile(r'\end{doc')
 closebracStr = re.compile(r'\}')
@@ -36,16 +38,20 @@ for i in range(1,7):
 					if not(commentStr.search(line)):
 						treatedLine = pictureStr.sub(pictureFold, addrStr.sub(nullChain, newlineStr.sub(nullChain, line)))
 						
-						if i == 1 && not(biblioStr.search(treatedLine) || enddocStr.search(treatedLine)):
+						if i == 1 && not(printbibStr.search(treatedLine) || enddocStr.search(treatedLine)):
 							echo @(treatedLine) >> Thesis.tex
 							if usepackageStr.search(treatedLine):
-								packages.append(usepackageStr.sub(nullChain, closebracStr.sub( nullChain, treatedLine)))
+								package = usepackageStr.sub(nullChain, closebracStr.sub( nullChain, treatedLine))
+								packages.append(package)
+								print("Package added: " + package)
 								lastPack+=1
 								
 						elif not(docclassStr.search(treatedLine) || makeatStr.search(treatedLine) || newcomStr.search(treatedLine) || begindocStr.search(treatedLine)):
 							if usepackageStr.search(treatedLine):
 								if not(any(s in line for s in packages)):
-									packages.append(usepackageStr.sub(nullChain, closebracStr.sub(nullChain, treatedLine)))
+									package = usepackageStr.sub(nullChain, closebracStr.sub( nullChain, treatedLine))
+									packages.append(package)
+									print("Package added: " + package)
 									countLine = 1
 									with open('Thesis.tex', 'r') as thesis:
 										for copLine in thesis:
@@ -60,7 +66,7 @@ for i in range(1,7):
 									rm Thesis.tex
 									mv copThesis.tex Thesis.tex
 								
-							elif i == 5:
+							elif i == 5 && not(bibresStr.search(treatedLine)):
 								echo @(treatedLine) >> Thesis.tex
 							
 							elif not(biblioStr.search(treatedLine) || enddocStr.search(treatedLine)):
@@ -70,7 +76,7 @@ for i in range(1,7):
 
 echo 'Starting latex compilation'
 pdflatex -synctex=1 -interaction=nonstopmode Thesis.tex
-bibtex Thesis.aux
+biber Thesis
 pdflatex -synctex=1 -interaction=nonstopmode Thesis.tex
 pdflatex -synctex=1 -interaction=nonstopmode Thesis.tex
 evince Thesis.pdf
