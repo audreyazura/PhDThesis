0f3e55fc0b4b3c738103298bafde7f5d52be7b05
audreyazura <azura.xiloscient@gmail.com>
2017-05-29 21:34:41 +0200

    Correction of Chap.04 suppression and windows compilation script done.

---
+++ CompileThesisWin.py
@@ -0,0 +1,85 @@
+#!/usr/local/bin/xonsh
+
+import re
+#import antigravity
+
+!(del Thesis.tex)
+print('Old Thesis.tex deleted')
+
+packages = []
+lastPack = 2
+
+nullChain = ''
+commentStr = re.compile(r'^%')
+pictureStr = re.compile(r"Pictures/")
+addrStr = re.compile(r"\.\./")
+newlineStr = re.compile(r"\n")
+usepackageStr = re.compile(r'\\usepackage(\[[\w=,]{1,50}\])?\{')
+biblioStr = re.compile(r'[b,B]iblio')
+printbibStr = re.compile(r'\\printbiblio')
+bibresStr = re.compile(r'\\addbibres')
+begindocStr = re.compile(r'\\begin{doc')
+enddocStr = re.compile(r'\\end{doc')
+closebracStr = re.compile(r'\}')
+docclassStr = re.compile(r'\\documentclass')
+makeatStr = re.compile(r'\\makeat')
+newcomStr = re.compile(r'\\renewcommand')
+quoteStr = re.compile(r'\"')
+
+with open('Thesis.tex', 'w') as Thesis:
+        for i in range(1,7):
+        	num = '0' + str(i)
+        	fold = re.search(num + '-[a-zA-z-]{1,10}', str(!(dir)))
+        	if fold:
+        		folder = fold.group()
+        		pictureFold = folder + '/Pictures/'
+        		cfile = re.search(num + '-[a-zA-Z-]{4,8}.tex', str(!(dir @(folder))))
+        		if cfile:
+        			searchedFile = folder + '/' + cfile.group()
+        			with open(searchedFile, 'r') as inpFile:
+        				for line in inpFile:
+        					if not(commentStr.search(line)):
+        						treatedLine = pictureStr.sub(pictureFold, addrStr.sub(nullChain, newlineStr.sub(nullChain, line)))
+                        				
+                        				if i == 1 && not(printbibStr.search(treatedLine) || enddocStr.search(treatedLine)):
+                        					print(treatedLine, file=Thesis)
+                        					if usepackageStr.search(treatedLine):
+                        						package = usepackageStr.sub(nullChain, closebracStr.sub( nullChain, treatedLine))
+                        						packages.append(package)
+                        						print('Package added: ' + package)
+                        						lastPack+=1
+                        						
+                        				elif not(docclassStr.search(treatedLine) || makeatStr.search(treatedLine) || newcomStr.search(treatedLine) || begindocStr.search(treatedLine)):
+                        					if usepackageStr.search(treatedLine):
+                        						if not(any(s in line for s in packages)):
+                        							package = usepackageStr.sub(nullChain, closebracStr.sub( nullChain, treatedLine))
+                        							packages.append(package)
+                        							print("Package added: " + package)
+                        							countLine = 1
+                        							with open('Thesis.tex', 'r') as originThesis:
+                                                                                        with open('copThesis.tex', 'w') as copiedThesis:
+                                                                                                for copLine in originThesis:
+                                                                                                	copTreatedLine = newlineStr.sub(nullChain, copLine)
+                                                                                                	if countLine == lastPack:
+                                                                                                		adLine = copTreatedLine + '\n' + treatedLine
+                                                                                                		print(adLine, file=copiedThesis) 
+                                                                                                	else:
+                                                                                                		print(copTreatedLine, file=copiedThesis)
+                                                                                                	countLine+=1
+                        							lastPack+=1
+                        							!(del Thesis.tex)
+                        							!(rename copThesis.tex Thesis.tex)
+                        						
+                        					elif i == 5 && not(bibresStr.search(treatedLine)):
+                        						print(treatedLine, file=Thesis)
+                        					
+                        					elif not(biblioStr.search(treatedLine) || enddocStr.search(treatedLine)):
+                        						print(treatedLine, file=Thesis)
+                        			
+                        	print('File ' + cfile.group() + ' done')
+
+print('Starting latex compilation')
+pdflatex -synctex=1 -interaction=nonstopmode Thesis.tex
+biber Thesis
+pdflatex -synctex=1 -interaction=nonstopmode Thesis.tex
+pdflatex -synctex=1 -interaction=nonstopmode Thesis.tex
